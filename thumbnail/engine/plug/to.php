788d518a5e93f50f6efc1ebe9a00212c1a72d6da
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-07 05:36:56 +0700

    Update

--- thumbnail/engine/plug/to.php
+++ thumbnail/engine/plug/to.php
@@ -3,7 +3,7 @@
 To::_('thumbnail', function(...$lot) use($url) {
     $in = array_shift($lot);
     if (!is_string($in)) return $in;
-    if (strpos($in, $u = To::url(ASSET) . '/') === 0) {
+    if (strpos($in, $u = To::URL(ASSET) . '/') === 0) {
         return str_replace($u, $url . '/' . Plugin::state('thumbnail', 'path') . '/' . implode('/', array_filter($lot)) . '/', $in);
     }
     return $in;
